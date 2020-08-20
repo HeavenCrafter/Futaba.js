99c44a9b03ca714f9de4ff2936c669b6df76548c
HeavenCrafter <thunderheavencrafter@gmail.com>
2020-08-20 23:36:47 +0800

    Revamping Handlers

--- bot.js
+++ bot.js
@@ -10,24 +10,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-["command"].forEach(handler => {
-  require(`./handlers/${handler}`)(client);
-});
-
-client.on("message", async message => {
-  if (message.author.bot) return;
-  if (!message.guild) return;
-  if (!message.content.startsWith(prefix)) return;
-  if (!message.member) message.member = await message.guild.fetchMember(message);
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const cmd = args.shift().toLowerCase();
-  if (cmd.length === 0) return;
-  let command = client.commands.get(cmd);
-  if (!command) command = client.commands.get(client.aliases.get(cmd));
-  if (command)
-    command.run(client, message, args);
-});
-
 client.on("message", async message => {
   if (message.content.toLowerCase().startsWith("oreo")) {
     message.react("🥞");
