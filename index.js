6224dc0bc1128908c135fbf6795861b25b20b7a6
HeavenCrafter <thunderheavencrafter@gmail.com>
2020-08-21 04:35:26 +0800

    Fully recoded the index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const futabaJS = require ('./handler/ClientBuilder.js')
+const client = new futabaJS();
+
+require("./handler/module.js")(client);
+require("./handler/Event.js")(client);
+
+client.package = require ("./package.json");
+client.on("warn", console.warn);
+client.on("error", console.error);
+client.login(process.env.BOT_TOKEN).catch(console.error);
