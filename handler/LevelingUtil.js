67a7d50ba932b77bca465ec11e72b3659b40c8f3
HeavenCrafter <thunderheavencrafter@gmail.com>
2020-08-20 23:48:08 +0800

    Added Leveling Event

---
+++ handler/LevelingUtil.js
@@ -0,0 +1,15 @@
+const moment = require("moment");
+
+this.getLevel = (xp) => {
+    return Math.floor(0.177 * Math.sqrt(xp)) + 1;
+}
+
+this.getLevelBounds = (level) => {
+    const lowerBound = Math.ceil(((level - 1) / 0.177) ** 2);
+    const upperBound = Math.ceil((level / 0.177) ** 2);
+    return { lowerBound, upperBound };
+}
+
+this.gainedXp = () => {
+    return Math.ceil(Math.random() * 9) + 3;
+}
