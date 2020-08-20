83a3f649b0cf6c80685f4f097260c96ac7c67667
HeavenCrafter <thunderheavencrafter@gmail.com>
2020-08-21 06:38:47 +0800

    Recoded the ready.js event
    I've recoded the entire thing from top to bottom to be more developer friendly
    
    So now it'll show everything that the specs.js command shows but instead of on command, it'll show it everytime it gets boot up online.

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,51 @@
+const os = require('os');
+const cpuStat = require('cpu-stat');
+const Discord = require("discord.js");
+
 module.exports = client => {
     console.log("The bot is ready!");
+
+    cpuStat.usagePercent(function (error, percent, seconds) {
+        if (error) {
+            return console.error(error)
+        }
+        const cores = os.cpus().length // Threads and Core counts
+        const cpuModel = os.cpus()[0].model // CPU Model
+        const guild = client.guilds.cache.size.toLocaleString() // Total Servers
+        const user = client.users.cache.size.toLocaleString() // Total Users
+        const channel = client.channels.cache.size.toLocaleString() // Total Channels
+        const usage = formatBytes(process.memoryUsage().heapUsed) // Mem Usage
+        const Node = process.version // Node Ver
+        const CPU = percent.toFixed(2) // CPU Usage
+        
+        console.log('Bot Statistics:', `Server: ${guild} \nUser: ${user} \nChannel: ${channel} \nUsage: ${usage} \nNode: ${Node} \nCPU Usage: ${CPU}%`) // Use Grave accent or `` 
+        console.log('Physical Statistics:', `CPU: ${cores} - ${cpuModel} \nUptime: ${parseDur(client.uptime)}`)
+    })
+}
+
+function formatBytes (a, b) {
+    if (0 == a) return "0 Bytes";
+    let c = 1024,
+        d = b || 2,
+        e = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"],
+        f = Math.floor(Math.log(a) / Math.log(c));
+    return parseFloat((a / Math.pow(c, f)).toFixed(d)) + " " + e[f]
+}
+
+function parseDur(ms) {
+    let seconds = ms / 1000,
+        days = parseInt(seconds / 86400);
+    seconds = seconds % 86400
+    let hours = parseInt(seconds / 3600);
+    seconds = seconds % 3600
+    let minutes = parseInt(seconds / 60);
+    seconds = parseInt(seconds % 60)
+    if (days) {
+        return `${days} day, ${hours} hours, ${minutes} minutes`
+    } else if (hours) {
+        return `${hours} hours, ${minutes} minutes, ${seconds} seconds`
+    } else if (minutes) {
+        return `${minutes} minutes, ${seconds} seconds`
+    }
+    return `${seconds} second(s)`
 }
