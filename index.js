e08b9e0c23fa3794a3b6960a929c9afe01520cfb
Heaven <thunderheavencrafter@gmail.com>
2020-08-21 06:03:08 +0000

    v2.1.0 Release

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+//Core Futaba Dependencies
 const Discord = require('discord.js');
 const futabaJS = require ('./handler/ClientBuilder.js')
 const client = new futabaJS();
+
+//Website Dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
 require("./handler/module.js")(client);
 require("./handler/Event.js")(client);
@@ -10,8 +17,14 @@ client.on("warn", console.warn);
 client.on("error", console.error);
 client.login(process.env.BOT_TOKEN).catch(console.error);
 
-client.on("message", async message => {
-    if (message.content.toLowerCase().startsWith("oreo")) {
-        message.react("🥞");
-    }
+//Express Website/Server
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('public/index.html');
 });
+
+app.listen(port, () => console.log(`Express server started successfully`));
